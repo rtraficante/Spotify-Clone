ead2fc1a649cb731a9b4ad052205d7d659bba650
RTraficante <rtraficante22@gmail.com>
2022-07-26 17:46:57 -0400

    added instructions to the starting page

---
+++ components/NotLoggedIn.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import React from "react";
+
+function NotLoggedIn() {
+  return (
+    <div className="mt-24 flex flex-col items-center">
+      <h1 className="mb-12 text-2xl text-center">
+        You Must Be Logged In To Access This Application
+      </h1>
+      <div className="border p-4 flex flex-col mb-8 w-[400px] min-w-[320px] rounded-md">
+        <h2 className="font-bold mb-4 text-xl self-center">Instructions</h2>
+        <p>
+          Please visit{" "}
+          <a className="hover:underline text-green-500" href="spotify.com">
+            Spotify
+          </a>{" "}
+          and log in using the account details below. Play one of the songs from
+          the demo account, then go to the Login page via the button below and
+          click Login. You will then have access to demo the app.
+        </p>
+      </div>
+
+      <div className="border p-4 flex flex-col mb-12 w-[400px] min-w-[320px] rounded-md">
+        <p className="self-center text-xl mb-4 font-bold">Demo Login Details</p>
+        <p className="mb-2">Email: musicappclonedemo@gmail.com</p>
+        <p className="">Password: clonedemo123</p>
+      </div>
+      <Link href="/login">
+        <button className="rounded-lg bg-green-500 p-4 w-full max-w-[400px]">
+          Go To Login Page
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default NotLoggedIn;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 
 import Center from "../components/Center";
+import NotLoggedIn from "../components/NotLoggedIn";
 import Player from "../components/Player";
 import Sidebar from "../components/Sidebar";
 
@@ -11,40 +12,25 @@ const Home = () => {
 
   return (
     <>
-      {session ? (
-        <div className="bg-black h-screen overflow-hidden">
-          <Head>
-            <title>Spotify 2.0</title>
-          </Head>
+      <div className="bg-black h-screen overflow-hidden">
+        <Head>
+          <title>Music App Clone</title>
+        </Head>
+        {session ? (
+          <>
+            <main className="flex">
+              <Sidebar />
+              <Center />
+            </main>
 
-          <main className="flex">
-            <Sidebar />
-            <Center />
-          </main>
-
-          <div className="sticky bottom-0">
-            <Player />
-          </div>
-        </div>
-      ) : (
-        <div className="mt-24 flex flex-col items-center">
-          <h1 className="mb-12 text-2xl text-center">
-            You Must Be Logged In To Access This Application
-          </h1>
-          <div className="border p-4 flex flex-col mb-12">
-            <p className="self-center text-xl mb-4 font-bold">
-              Demo Login Details
-            </p>
-            <p className="mb-2">Email: musicappclonedemo@gmail.com</p>
-            <p className="">Password: clonedemo123</p>
-          </div>
-          <Link href="/login">
-            <button className="rounded-lg bg-green-500 p-4">
-              Go To Login Page
-            </button>
-          </Link>
-        </div>
-      )}
+            <div className="sticky bottom-0">
+              <Player />
+            </div>
+          </>
+        ) : (
+          <NotLoggedIn />
+        )}
+      </div>
     </>
   );
 };
